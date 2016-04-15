5a7b2ef1bcb0865d6dda6aa1429bab3257fef32b
FrederikLJensen <flj_1500@hotmail.com>
2016-04-15 11:42:16 +0200

    Bluetooth service has GATT implemented. Notification working.

--- BluetoothService.java
+++ BluetoothService.java
@@ -46,7 +46,13 @@ public class BluetoothService extends Service {
     // This is for broadcasting with extra data attached.
     public final static String EXTRA_DATA = "com.vaegtregistreringaffysiskbelastning.bluetooth.le.EXTRA_DATA";
 
-
+    private BroadcastReceiver BLEScanReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(TAG, "BluetoothLEScanRequest Received: " + intent.toString());
+            //scanLeDevice();
+        }
+    };
 
     // Initializes Bluetooth manager.
 
@@ -61,13 +67,7 @@ public class BluetoothService extends Service {
         mBluetoothAdapter = bluetoothManager.getAdapter();
         // Register receiver for getting updates on bluetooth devices
         IntentFilter filter = new IntentFilter("com.vaegtregistreringaffysiskbelasting.BLEScan");
-        this.registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Log.d(TAG, "BluetoothLEScanRequest Received: " + intent.toString());
-                //scanLeDevice();
-            }
-        }, filter);
+        this.registerReceiver(BLEScanReceiver, filter);
         scanLeDevice();
     }
 
@@ -149,8 +149,9 @@ public class BluetoothService extends Service {
                             for (BluetoothGattCharacteristic gattCha : gattserv.getCharacteristics()) {
                                 Log.d(TAG, "onServicesDiscovered, Service, Characteristics: " + gattCha.getUuid() + " Desc: " + gattCha.getDescriptor(gattCha.getUuid()));
                                 // If the characteristic contains "beef" (our UUID for it) then read it.
+                                
                                 if(gattCha.getUuid().toString().contains("beef")) {
-                                    Log.d(TAG, "onServicesDiscovered, Service, Characteristics reading...: " + gatt.readCharacteristic(gattCha));
+                                    //Log.d(TAG, "onServicesDiscovered, Service, Characteristics reading...: " + gatt.readCharacteristic(gattCha));
                                     mBluetoothGatt.setCharacteristicNotification(gattCha, true);
 
                                     // Iterate through all the descriptors in the characteristic. We need to set ENABLE_NOTIFICATION_VALUE which is 0x2902:
@@ -160,7 +161,7 @@ public class BluetoothService extends Service {
                                         // If the descriptor is the place to write ENABLE_NOTIFICATION_VALUE
                                         if(descriptor.getUuid().toString().contains("2902")){
                                             descriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
-                                            mBluetoothGatt.writeDescriptor(descriptor);
+                                            Log.d(TAG, "onServicesDiscovered: Descriptor written: " + mBluetoothGatt.writeDescriptor(descriptor));
                                         }
                                     }
 
@@ -214,7 +215,7 @@ public class BluetoothService extends Service {
     @Override
     public void onDestroy(){
         Log.d(TAG, "Bluetooth Service Destroyed!");
-
+        unregisterReceiver(BLEScanReceiver);
         super.onDestroy();
     }
 
